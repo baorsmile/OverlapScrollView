192d117962f35cbc82db7a46998a67d50a540a90
田大宝 <tiandabao@le.com>
2017-07-18 15:59:59 +0800

    first update

--- OverlapScrollView/ViewController.m
+++ OverlapScrollView/ViewController.m
@@ -8,8 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define w [UIScreen mainScreen].bounds.size.width
+#define h [UIScreen mainScreen].bounds.size.height
 
+@interface ViewController ()
+@property (nonatomic, strong) UIScrollView *mainScrollView;
+@property (nonatomic, strong) UIScrollView *verticalScrollView;
 @end
 
 @implementation ViewController
@@ -17,6 +21,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    [self.view addSubview:self.verticalScrollView];
+    
+    for (NSInteger i = 0; i < 2; i ++) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, h * i, w, h)];
+        
+        if (i == 0) {
+            view.backgroundColor = [UIColor orangeColor];
+        }
+        else {
+            view.backgroundColor = [UIColor lightGrayColor];
+        }
+        
+        [self.verticalScrollView addSubview:view];
+    }
+    
+    [self.view addSubview:self.mainScrollView];
+    
+    for (NSInteger i = 0; i < 2; i ++) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(w * i, 0, w, h)];
+        
+        if (i == 0) {
+            view.backgroundColor = [UIColor clearColor];
+        }
+        else {
+            view.backgroundColor = [UIColor redColor];
+            [self.mainScrollView addSubview:view];
+        }
+        
+    }
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureRecognizer:)];
+    [self.view addGestureRecognizer:pan];
 }
 
 
@@ -25,5 +62,59 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)panGestureRecognizer:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint translation = [recognizer translationInView:recognizer.view];
+    
+    CGPoint velocity = [recognizer velocityInView:recognizer.view];
+    
+    static BOOL isVertical;
+    static CGPoint currentContentOff;
+    
+    switch (recognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            if(fabs(velocity.x) > fabs(velocity.y)) {
+                isVertical = NO;
+            } else {
+                currentContentOff = self.verticalScrollView.contentOffset;
+                isVertical = YES;
+            }
+            break;
+        case UIGestureRecognizerStateChanged:
+            
+            if (isVertical) {
+                self.verticalScrollView.contentOffset = CGPointMake(0, currentContentOff.y - translation.y);
+            }
+            
+            break;
+        case UIGestureRecognizerStateEnded:
+            if (isVertical) {
+                [self.verticalScrollView setContentOffset:CGPointMake(0,  h - currentContentOff.y) animated:YES];
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+- (UIScrollView *)mainScrollView
+{
+    if (!_mainScrollView) {
+        _mainScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, w, h)];
+        _mainScrollView.contentSize = CGSizeMake(w * 2, h);
+        _mainScrollView.pagingEnabled = YES;
+    }
+    return _mainScrollView;
+}
+
+- (UIScrollView *)verticalScrollView
+{
+    if (!_verticalScrollView) {
+        _verticalScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, w, h)];
+        _verticalScrollView.contentSize = CGSizeMake(w, h * 2);
+        _verticalScrollView.pagingEnabled = YES;
+    }
+    return _verticalScrollView;
+}
 
 @end
---
+++ README.md
@@ -0,0 +1,3 @@
+# OverlapScrollView
+
+## Two UIScrollView to overlap, mainScrollView is on the verticalScrollView, when scroll to vertical, the verticalScrollView scroll!!!
